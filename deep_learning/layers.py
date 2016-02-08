ce7463c9d271b4878548b15f031c0c55c8d7f395
ShuaiW <info.shuai@gmail.com>
2016-02-08 13:38:23 -0500

    added naive implementation of conv, pool, both forward and backward; also added spatial batchnorm

--- deep_learning/layers.py
+++ deep_learning/layers.py
@@ -151,7 +151,7 @@ def batchnorm_forward(x, gamma, beta, bn_param):
   bn_param['running_var'] = running_var
   
   # necessary cache for back prop
-  cache = x, norm, gamma, sample_mean, sample_var, eps
+  cache = x, norm, gamma, running_mean, running_var, eps
 
   return out, cache
 
@@ -271,6 +271,214 @@ def dropout_backward(dout, cache):
 
   return dx
 
+
+def conv_forward_naive(x, w, b, conv_param):
+  """
+  A naive implementation of the forward pass for a convolutional layer.
+
+  The input consists of N data points, each with C channels, height H and width
+  W. We convolve each input with F different filters, where each filter spans
+  all C channels and has height HH and width HH.
+
+  Input:
+  - x: Input data of shape (N, C, H, W)
+  - w: Filter weights of shape (F, C, HH, WW)
+  - b: Biases, of shape (F,); one bias for each filter
+  - conv_param: A dictionary with the following keys:
+    - 'stride': The number of pixels between adjacent receptive fields in the
+      horizontal and vertical directions.
+    - 'pad': The number of pixels that will be used to zero-pad the input.
+
+  Returns a tuple of:
+  - out: Output data, of shape (N, F, H', W') where H' and W' are given by
+    H' = 1 + (H + 2 * pad - HH) / stride
+    W' = 1 + (W + 2 * pad - WW) / stride
+  - cache: (x, w, b, conv_param)
+  """
+  stride, pad = conv_param['stride'], conv_param['pad']
+  N, C, H, W = x.shape
+  F, C, HH, WW = w.shape
+  H_out = int(1 + (H + 2 * pad - HH) / stride)
+  W_out = int(1 + (W + 2 * pad - WW) / stride)
+  
+  out = np.zeros((N, F, H_out, W_out))
+  
+  # pad along H, W dimensions
+  pad_dim = ( (0, 0), (0, 0), (pad, pad), (pad, pad) )
+  x_pad = np.pad(x, pad_width=pad_dim, mode='constant', constant_values=0)
+
+  for n in range(N):
+    for f in range(F):
+      for hei in range(H_out):
+        for wid in range(W_out):
+          out[n, f, hei, wid] = np.sum(x_pad[n, :, hei*stride:hei*stride+HH,\
+              wid*stride: wid*stride+WW] * w[f]) + b[f]
+
+  cache = (x_pad, w, b, conv_param)
+  return out, cache
+
+
+def conv_backward_naive(dout, cache):
+  """
+  A naive implementation of the backward pass for a convolutional layer.
+
+  Inputs:
+  - dout: Upstream derivatives.
+  - cache: A tuple of (x, w, b, conv_param) as in conv_forward_naive
+
+  Returns a tuple of:
+  - dx: Gradient with respect to x
+  - dw: Gradient with respect to w
+  - db: Gradient with respect to b
+  """
+  (x_pad, w, b, conv_param) = cache
+  stride, pad = conv_param['stride'], conv_param['pad']
+  
+  F, C, HH, WW = w.shape
+  N, F, H_out, W_out = dout.shape
+  
+  # dout: (N, F, H_out, W_out); b: (F, ); w: (F, C, HH, WW); x: (N, C, H, W)
+  db = np.sum(dout, axis=(0, 2, 3))
+  
+  dw = np.zeros((w.shape))
+  dx = np.zeros((x_pad.shape))
+  
+  for n in range(N):
+    for f in range(F):
+      for hei in range(H_out):
+        for wid in range(W_out):
+          # update dw
+          dw[f] += x_pad[n, :, hei*stride:hei*stride+HH,
+              wid*stride: wid*stride+WW] * dout[n, f, hei, wid]
+          # update dx
+          dx[n, :, hei*stride:hei*stride+HH, wid*stride: wid*stride+WW] += \
+              w[f] * dout[n, f, hei, wid]
+  # 'unpad' dx
+  dx = dx[:, :, pad:-pad, pad:-pad]
+  
+  return dx, dw, db
+
+
+def max_pool_forward_naive(x, pool_param):
+  """
+  A naive implementation of the forward pass for a max pooling layer.
+
+  Inputs:
+  - x: Input data, of shape (N, C, H, W)
+  - pool_param: dictionary with the following keys:
+    - 'pool_height': The height of each pooling region
+    - 'pool_width': The width of each pooling region
+    - 'stride': The distance between adjacent pooling regions
+
+  Returns a tuple of:
+  - out: Output data
+  - cache: (x, pool_param)
+  """
+  ph = pool_param['pool_height']
+  pw = pool_param['pool_width']
+  stride = pool_param['stride']
+  
+  N, C, H, W = x.shape
+  
+  out = np.zeros((N, C, H/ph, W/pw))
+  
+  for h in range(H):
+    for w in range(W):
+      try:
+        out[:, :, h, w] = np.max(x[:, :, h*stride:h*stride+ph, 
+            w*stride:w*stride+pw], axis=(2, 3))
+      except ValueError:
+        pass
+      
+  cache = (x, pool_param)
+  return out, cache
+
+
+def max_pool_backward_naive(dout, cache):
+  """
+  A naive implementation of the backward pass for a max pooling layer.
+
+  Inputs:
+  - dout: Upstream derivatives
+  - cache: A tuple of (x, pool_param) as in the forward pass.
+
+  Returns:
+  - dx: Gradient with respect to x
+  """
+  x, pool_param = cache
+  ph = pool_param['pool_height']
+  pw = pool_param['pool_width']
+  stride = pool_param['stride']
+
+  dx = np.zeros((x.shape))
+  N, C, H, W = x.shape
+  for n in range(N):
+    for c in range(C):
+      for h in range(H):
+        for w in range(W):
+          try:
+            arg_max = x[n, c, h*stride:h*stride+ph, 
+                        w*stride:w*stride+pw].argmax()
+            idx = np.unravel_index(arg_max, (ph, pw))
+            dx[n, c, h*stride:h*stride+ph, w*stride:w*stride+pw][idx] = \
+                dout[n, c, h, w]
+          except ValueError:
+            pass
+  return dx
+
+
+def spatial_batchnorm_forward(x, gamma, beta, bn_param):
+  """
+  Computes the forward pass for spatial batch normalization.
+  Implemeted using batchnorm_forward() above.
+  
+  Inputs:
+  - x: Input data of shape (N, C, H, W)
+  - gamma: Scale parameter, of shape (C,)
+  - beta: Shift parameter, of shape (C,)
+  - bn_param: Dictionary with the following keys:
+    - mode: 'train' or 'test'; required
+    - eps: Constant for numeric stability
+    - momentum: Constant for running mean / variance. momentum=0 means that
+      old information is discarded completely at every time step, while
+      momentum=1 means that new information is never incorporated. The
+      default of momentum=0.9 should work well in most situations.
+    - running_mean: Array of shape (C,) giving running mean of features
+    - running_var Array of shape (C,) giving running variance of features
+    
+  Returns a tuple of:
+  - out: Output data, of shape (N, C, H, W)
+  - cache: Values needed for the backward pass
+  """
+  N, C, H, W = x.shape
+  x_2d = x.transpose(0, 2, 3, 1).reshape(N*H*W, C)
+  out, cache = batchnorm_forward(x_2d, gamma, beta, bn_param)
+  out = out.reshape(N, H, W, C).transpose(0, 3, 1, 2)
+
+  return out, cache
+
+
+def spatial_batchnorm_backward(dout, cache):
+  """
+  Computes the backward pass for spatial batch normalization.
+  Implemeted using batchnorm_backward_alt above.
+  
+  Inputs:
+  - dout: Upstream derivatives, of shape (N, C, H, W)
+  - cache: Values from the forward pass
+  
+  Returns a tuple of:
+  - dx: Gradient with respect to inputs, of shape (N, C, H, W)
+  - dgamma: Gradient with respect to scale parameter, of shape (C,)
+  - dbeta: Gradient with respect to shift parameter, of shape (C,)
+  """
+  N, C, H, W = dout.shape
+  dout_2d = dout.transpose(0, 2, 3, 1).reshape(N*H*W, C)
+  dx_2d, dgamma, dbeta = batchnorm_backward_alt(dout_2d, cache)
+  dx = dx_2d.reshape(N, H, W, C).transpose(0, 3, 1, 2)
+  
+  return dx, dgamma, dbeta
+  
 
 
 def svm_loss(x, y):
